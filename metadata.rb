f7f178fa664efc722f2c7428db7b967b9645acaf
Robert Coleman <github@robert.net.nz>
2014-05-23 13:12:19 +1200

    Version bump to 0.2.1

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email 'cookbooks@robert.net.nz'
 license          'MIT'
 description      'Installs/Configures n98-magerun.phar'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.0'
+version          '0.2.1'
 name             'n98-magerun'
 recipe           'n98-magerun', 'Installs n98-magerun'
 
